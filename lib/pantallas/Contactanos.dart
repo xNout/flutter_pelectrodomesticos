4e4514ea5ace55ddb5927a03b8fee391bd6ee1fa
Adrian Vera <vera254d4@gmail.com>
2024-07-28 16:04:05 -0500

    modificado formulario contacto, se agrego layout

--- lib/pantallas/Contactanos.dart
+++ lib/pantallas/Contactanos.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../layout/MainLayout.dart';
+
 class Contactanos extends StatelessWidget {
   const Contactanos({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contáctanos'),
-      ),
-      body: Center(
-        child: Text('Contenido de Contáctanos'),
+    return const MainLayout(
+      content: Center(
+        child: Text('Contenido de la página contacto'),
       ),
     );
   }
